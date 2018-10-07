1377bac8468a63595ff05714a789a73f82a0ccd9
DenyingTheTruth <anton.bachikin@gmail.com>
2018-10-07 22:40:09 +0300

    feat: function longestConsecutiveLength

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function longestConsecutiveLength(array) {
-  // your solution here
+
+  let sequence = new Set();
+  let length = 0;
+  let i;
+  let sequence_value;
+
+  for (i = 0; i < array.length; i++) sequence.add(array[i]);
+
+  for (i = 0; i < array.length; i++) {
+    if (!sequence.has(array[i] - 1)) {
+      sequence_value = array[i];
+      while (sequence.has(sequence_value)) sequence_value++;
+
+      if (length < (sequence_value - array[i])) length = sequence_value - array[i];
+    }
+  }
+  return length;
 }
